cd270385d52b7af86f082046018852cd3de62c83
Kings-Israel <milimokings@gmail.com>
2026-01-11 13:57:16 +0300

    Update

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    info('Welcome to the Grocery API');
+    dd('Welcome to the Grocery API');
     return view('welcome');
 });
