d3cb8e4f84310ac3ed67a20da90573711a158a94
Kings-Israel <milimokings@gmail.com>
2026-01-12 07:59:47 +0300

    Update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -90,7 +90,7 @@ class AuthController extends Controller
         }
 
         $token = $user->createToken('auth_token')->plainTextToken;
-
+        
         return response()->json([
             'user' => $user,
             'token' => $token
--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,7 @@ class User extends Authenticatable
             'password' => 'hashed',
             'latitude' => 'decimal:8',
             'longitude' => 'decimal:8',
+            'phone_verified' => 'boolean'
         ];
     }
 
